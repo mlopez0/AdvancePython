4fdd3b1f042277f76da9c3657f52f3a62e3a5a91
Semenov Vladimir Alekseevich <ext.vsemenov@tinkoff.ru>
2020-05-30 18:42:58 +0300

    fix handle_cd

---
+++ A1/ex4.py
@@ -0,0 +1,35 @@
+'''
+Marvin Lopez
+Vladimir Semenov
+
+Assignment 1
+"Command line hero"
+
+Python 3.8.0
+'''
+
+import os
+from A1.common_functions import *
+
+dir_path = ""
+
+while True:
+    dir_path = get_path_abbreviation(os.getcwd())
+
+    print("myshell [" + dir_path + "]: ", end="")
+
+    try:
+        _input = input()
+    except EOFError:
+        print()
+        exit_terminal()
+
+    if _input == "exit":
+        exit_terminal()
+
+    breakdown = _input.split(" ")
+
+    if handle_cd(breakdown) > 0:
+        continue
+
+    os.system(_input)
