722a93d801b44674a058ae08a5423258666e9899
m.lopez@innopolis.university <m.lopez@innopolis.university>
2020-06-01 12:37:27 +0300

    Pid issues fixed

--- A1/ex3.py
+++ A1/ex3.py
@@ -34,14 +34,18 @@ while True:
     if handle_cd(_input):
         continue
 
-    outcome = run(_input, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
-    pid = Popen("ls") # must be changed
-    pid = pid.pid
+    outcome = run(_input + " & echo $!", stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
 
-'''    print ("This is the comand \t" + str(_input))
-    print ("These are the args \t" + outcome.args)
-    print ("This is the output \t " + outcome.stdout)
-    print ("This is the exit code \t" + str(outcome.returncode))
-'''
-    log_string = str(_input) + "-*-" + outcome.args + "-*-" + outcome.stdout + '-*-' + str(pid) + "-*-" + str(outcome.returncode)
+    _command = _input.split(' ', 1)[0]                  # Return the command 
+
+    try:
+        _argument = _input.split(' ', 1)[1]             # Return the command arguments
+    except IndexError:
+        _argument = '-'
+    _pid = outcome.stdout.partition('\n')[0]            # Return the Pid
+    _output_ = outcome.stdout.replace(_pid, '')         # Return the output
+ 
+    print (_output_)
+
+    log_string = _command + "-*-" + _argument + "-*-" + _output_ + '-*-' + _pid + "-*-" + str(outcome.returncode)
     action_log(log_string)
