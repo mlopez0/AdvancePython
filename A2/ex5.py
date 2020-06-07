77096e7d67cc5c61591ce777cc85234dfe02c603
Semenov Vladimir Alekseevich <ext.vsemenov@tinkoff.ru>
2020-06-07 22:41:24 +0300

    add table builder for ex5.py

--- A2/ex5.py
+++ A2/ex5.py
@@ -14,7 +14,7 @@ import subprocess
 from collections import OrderedDict
 import py_compile
 import getopt
-import io
+import dis
 
 FNULL = open(os.devnull, "w")
 
@@ -27,6 +27,7 @@ def throw_syntax_error():
     print("\t-py file.py\tcompile file into bytecode and store it as file.pyc")
     print("\t-s \"src\"\tcompile src into bytecode and store it as out.pyc")
     exit(2)
+
 
 def clean_temp_files():
     if os.path.exists("__tmp__.py"):
@@ -59,16 +60,55 @@ def print_mode(arguments):
     pass
 
 
+def compare_mode(files):
+    table = {filename: {} for filename in files}
+
+    for file in files:
+        code = open(file, "r")
+        bytecode = dis.Bytecode(code.read())
+        code.close()
+        
+        for instr in bytecode:
+            same_opnames = [instruction.opname for instruction in bytecode if instruction.opname == instr.opname]
+            table[file][instr.opname] = len(same_opnames)
+    
+    print_table(table)
+
+
+def print_table(table_dict):
+    opnames = set().union(*[opnames for opnames in table_dict.values()])
+    print(fill("INSTRUCTION"), end="")
+    for filename in table_dict.keys():
+        print(fill("| " + filename), end="")
+
+    print()
+    
+    for opname in opnames:
+        print(fill(opname), end="")
+        for filename in table_dict.keys():
+            num = "0"
+            if opname in table_dict[filename].keys():
+                num = str(table_dict[filename][opname])
+            print(fill("  " + num), end="")
+
+        print()
+
+def fill(line, cell_width=13):
+    return line + (" " * (cell_width - len(line))) if len(line) < cell_width else line[:cell_width]
+
+
 def parse_options(arguments):
     if arguments[0] == "compile":
         compile_mode(arguments[1:])
     elif arguments[0] == "print":
         print_mode(arguments[1:])
+    elif arguments[0] == "compare" and arguments[1] == "-py":
+        compare_mode(arguments[2:])
     else:
         throw_syntax_error()
 
 
-if (len(sys.argv) == 1) or sys.argv[1] not in ("compile", "print"):
+if (len(sys.argv) == 1) or sys.argv[1] not in ("compile", "print", "compare"):
     throw_syntax_error()
 
 
