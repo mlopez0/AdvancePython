6b9200fd4e5a1ebbd9ab869f3dcdc03634fb7669
m.lopez@innopolis.university <m.lopez@innopolis.university>
2020-06-07 14:35:49 +0300

    initial function ex2

---
+++ A2/ex2.py
@@ -0,0 +1,29 @@
+
+'''
+Marvin Lopez
+Vladimir Semenov
+
+Assignment 2
+"Russian Hacker"
+
+Python 3.8.0
+'''
+
+import dis
+import sys
+import os
+from collections import OrderedDict 
+import importlib
+
+_nfiles = len(sys.argv)
+_files = sys.argv[1:]
+_filesList = {}
+
+
+if (_nfiles >1):
+    _file = _files[0].replace(".py","") 
+    dis.dis(__import__(_file))
+
+else:
+    print ("usage: ex2.py -py src.py")
+    print ("This program...")
