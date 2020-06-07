55c970560630cb6579e4efd6842e85cc5e8957f3
Semenov Vladimir Alekseevich <ext.vsemenov@tinkoff.ru>
2020-06-07 21:34:49 +0300

    add ex4.py final

--- A2/ex4.py
+++ A2/ex4.py
@@ -14,12 +14,13 @@ import subprocess
 from collections import OrderedDict
 import py_compile
 import getopt
+import io
 
 FNULL = open(os.devnull, "w")
 
-_short_flags = "p:"
+_short_flags = "p:s:"
 
-def check_no_arguments():
+def throw_syntax_error():
     print(sys.argv[0] + " action [-flag value]")
     print("\nThis program ...\n")
     print("compile")
@@ -27,16 +28,47 @@ def check_no_arguments():
     print("\t-s \"src\"\tcompile src into bytecode and store it as out.pyc")
     exit(2)
 
-if (len(sys.argv) == 1) or sys.argv[1] not in ("compile", "print"):
-    check_no_arguments()
+def clean_temp_files():
+    if os.path.exists("__tmp__.py"):
+        os.remove("__tmp__.py")
 
-try:
-    arguments, values = getopt.getopt(sys.argv[2:], _short_flags)
-    if sys.argv[1] == "compile":
-        for argument, value in arguments:
-            if argument in ("-p"):
-                py_compile.compile(value, value.split(".")[0] + ".pyc")
-            elif argument in ("-s"):
-                pass
-except getopt.GetoptError:
-    check_no_arguments()
+
+def compile_string(string):
+    outputFile = "out.pyc"
+    subprocess.call('echo "' + string + '" > __tmp__.py', shell=True)
+    py_compile.compile("__tmp__.py", outputFile)
+    clean_temp_files()
+
+
+def compile_file(filename):
+    outputFile = filename.split(".")[0] + ".pyc"
+    py_compile.compile(filename, outputFile)
+
+
+def compile_mode(arguments):
+    if arguments[0] == "-py":
+        compile_file(arguments[1])
+    elif arguments[0] == "-s":
+        compile_string(arguments[1])
+    else:
+        throw_syntax_error()
+
+
+def print_mode(arguments):
+    pass
+
+
+def parse_options(arguments):
+    if arguments[0] == "compile":
+        compile_mode(arguments[1:])
+    elif arguments[0] == "print":
+        print_mode(arguments[1:])
+    else:
+        throw_syntax_error()
+
+
+if (len(sys.argv) == 1) or sys.argv[1] not in ("compile", "print"):
+    throw_syntax_error()
+
+
+parse_options(sys.argv[1:])
