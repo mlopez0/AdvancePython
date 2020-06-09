7333bd6d34e97bf3d3c70ec617aed301161cf4cc
Semenov Vladimir Alekseevich <ext.vsemenov@tinkoff.ru>
2020-06-09 12:17:33 +0300

    add reflect decorator

--- A3/ex1.py
+++ A3/ex1.py
@@ -8,13 +8,22 @@ Assignment 3
 Python 3.8.0
 '''
 
+import inspect
+# print (open(__file__).read()) # smallest python program that can print its own source code but it is not a quine because a quine should not use open() function to print out its source code.
 
-print (open(__file__).read()) # smallest python program that can print its own source code but it is not a quine because a quine should not use open() function to print out its source code.
+# import os
+# os.system("cat " + __file__)
+
+def reflect(function):
+    print(inspect.getsource(function).replace("@reflect\n", ""))
+    return function
 
 
+@reflect
 def foo():
     print("bar")
-    
+
+
 if __name__ == "__main__":
     foo()
     
